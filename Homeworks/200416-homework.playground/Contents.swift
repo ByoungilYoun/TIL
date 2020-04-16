fd0fe177d16b35d4d1034269bca1201aafc73fe2
ByoungilYoun <jimmy7525@naver.com>
2020-04-16 20:14:44 +0900

    feat : Include other answers about Enum & Optionals

--- Homeworks/200416-homework.playground/Contents.swift
+++ Homeworks/200416-homework.playground/Contents.swift
@@ -35,6 +35,40 @@ func combineString(str1: String?, str2: String?, str3: String?) -> String {
 combineString(str1: nil, str2: "ABC", str3: "DEF")
 combineString(str1: "ABC", str2: nil, str3: "KFC")
 combineString(str1: nil, str2: nil, str3: "ABC")
+
+// 다른 풀이
+// Optional Binding을 이용한 방식
+func combineString1(str1: String?, str2: String?, str3: String?) -> String {
+  var combinedString = ""
+  if let unwrappedString = str1 {
+    combinedString += unwrappedString
+  }
+  if let unwrappedString = str2 {
+    combinedString += unwrappedString
+  }
+  if let unwrappedString = str3 {
+    combinedString += unwrappedString
+  }
+  return combinedString
+}
+
+// Nil-coalescing Operator 를 이용한 방식
+func combineString2(str1: String?, str2: String?, str3: String?) -> String {
+  let string1 = str1 ?? ""
+  let string2 = str2 ?? ""
+  let string3 = str3 ?? ""
+  return string1 + string2 + string3
+}
+
+combineString1(str1: "AB", str2: "CD", str3: "EF")
+combineString1(str1: "AB", str2: nil, str3: "EF")
+combineString2(str1: "AB", str2: "CD", str3: "EF")
+combineString2(str1: "AB", str2: nil, str3: "EF")
+
+
+
+
+
 //2. 사칙연산(+, -, *, /)을 가진 enum 타입 Arithmetic과 2개의 자연수를 입력 파라미터로 받아 (파라미터 총 3개) 해당 연산의 결과를 반환하는 함수 구현
 //enum Arithmetic {
 //  case addition, subtraction, multiplication, division
@@ -70,6 +104,30 @@ func calculateNums (input : (Int, Int, Arithmetic)) -> Int {
 
 calculateNums(input: (100,10,.multiplication))
 
+//다른 풀이
+//enum Arithmetic {
+//  case addition, subtraction, multiplication, division
+//}
+//
+//func calculator(operand1: Int, operand2: Int, op: Arithmetic) -> Int {
+//  switch op {
+//  case .addition:
+//    return operand1 + operand2
+//  case .subtraction:
+//    return operand1 - operand2
+//  case .multiplication:
+//    return operand1 * operand2
+//  case .division:
+//    return Int(operand1 / operand2)
+//  }
+//}
+//
+//print(calculator(operand1: 15, operand2: 25, op: .addition))
+//print(calculator(operand1: 30, operand2: 10, op: .subtraction))
+//print(calculator(operand1: 12, operand2: 10, op: .multiplication))
+//print(calculator(operand1: 24, operand2: 6, op: .division))
+
+
 
 
 
@@ -85,7 +143,6 @@ calculateNums(input: (100,10,.multiplication))
 //enum Temperature {
 //  // 코드
 //}
-//
 
 enum Temperature {
     case celcius(Double)
@@ -108,3 +165,32 @@ enum Temperature {
 
 let getAtemperature = Temperature.fahrenheit(37.5)
 getAtemperature.toCelcius(x: 37.5)
+
+
+// 다른 풀이
+//enum Temperature {
+//  case celcius(Double)
+//  case fahrenheit(Double)
+//  case kelvin(Double)
+//
+//  func toCelcius() -> Double {
+//    switch self {
+//    case .celcius(let value):
+//        return value
+//    case .fahrenheit(let value):
+//        return (value - 32) * 5 / 9
+//    case .kelvin(let value):
+//        return value + 273
+//    }
+//  }
+//}
+//
+//var temperature: Temperature = .celcius(30)
+//temperature.toCelcius()
+//
+//temperature = .fahrenheit(86)
+//temperature.toCelcius()
+//
+//temperature = .kelvin(-273)
+//temperature.toCelcius()
+
