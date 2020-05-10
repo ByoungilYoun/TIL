68b2c85dbead7c550472a46396f57b3a6155ebcd
ByoungilYoun <jimmy7525@naver.com>
2020-05-10 16:36:43 +0900

    feat : 200508 project

---
+++ Homeworks/projects/TabBarControllerExample/TabBarControllerExample/SceneDelegate.swift
@@ -0,0 +1,73 @@
+//
+//  SceneDelegate.swift
+//  TabBarControllerExample
+//
+//  Created by 윤병일 on 2020/05/08.
+//  Copyright © 2020 Byoungil Youn. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+     
+        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let vc = ViewController()
+        let secondVC = SecondViewController()
+        let thirdVC = ThirdViewController()
+        
+        let naviVC = UINavigationController(rootViewController: thirdVC)
+        
+        let tabBarController = UITabBarController()
+        tabBarController.viewControllers = [vc, secondVC, naviVC]
+        
+        vc.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 0)
+        
+        // sf symbol 에서 가져다 쓰는거다. person.circle
+        secondVC.tabBarItem = UITabBarItem(title: "Second", image: UIImage(systemName: "person.circle"), tag: 1)
+        
+        naviVC.tabBarItem = UITabBarItem(title: "Third", image: UIImage(systemName: "trash"), tag: 2)
+        
+        
+        window?.rootViewController = tabBarController
+        window?.backgroundColor = .systemBackground
+        window?.makeKeyAndVisible()
+       
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+    }
+
+
+}
+
---
+++ Homeworks/projects/TabBarControllerExample/TabBarControllerExample/SecondViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SecondViewController.swift
+//  TabBarControllerExample
+//
+//  Created by 윤병일 on 2020/05/08.
+//  Copyright © 2020 Byoungil Youn. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemGreen
+       
+    }
+    
+
+
+}
